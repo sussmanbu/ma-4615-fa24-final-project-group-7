233744cc0f703582ed3295fc52cebabbbbefa645
Zexian Xu <jessiezx@bu.edu#>
2024-11-06 22:05:01 -0500

    Built site for gh-pages

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -12,17 +12,38 @@ policeContactData <- read_tsv("dataset/police-contact-data.tsv")
 numNaPerVariable <- policeContactData |>
   summarise(across(everything(), ~ sum(is.na(.)), .names = "na_count_{col}"))
 
+# Here we remove columns we don't want
+# Removed columns:
+# PSTRATA: don't understand how to use it
+# SECUCODE: not necessary
+# num_fu_HHint, num_fu_perint: don't need to know number of follow-ups
+# Columns that don't end with "_sub" but have a "_sub" counterpart: the "_sub" columns are objectively better -- they contain more data 
+# time2vic_inc_P23PER: don't want to investigate months from PPCS interview to victimization
+# 
+# QUESTIONS
+# There are many columns titled whyno[number]_inc_P23[HH for household crime, PER for personal crime] with 
+# description of Reason not reported: (insert reason), should I remove any of them?
+
 filtered <- policeContactData |>
   select(!ends_with("_sub"), !starts_with("vicar_")) |>
-  select(!c(SECUCODE, WEIGHT, NUM_FU_HHINT, NUM_FU_PERINT, PSTRATA, TIME2VIC_INC_P23PER)) 
-  select(!c(SECUCODE, NUM_FU_HHINT, NUM_FU_PERINT, PSTRATA, ends_with("_sub"), starts_with(("vicar_")), TIME2VIC_INC_P23PER)) 
-  #select(where(~sum(is.na(.x)) < 90000)) |>
+  select(!c(SECUCODE, WEIGHT, NUM_FU_HHINT, NUM_FU_PERINT, PSTRATA, TIME2VIC_INC_P23PER)) |>
+  mutate(
+    RACE = case_when(
+      C4_RACE == 1 ~ "White, Non-Hispanic",
+      C4_RACE == 2 ~ "Black, Non-Hispanic",
+      C4_RACE == 3 ~ "Hispanic",
+      C4_RACE == 4 ~ "Other or multiracial, Non-Hispanic"
+    )
+  ) |>
+  relocate(RACE, .before = AGE)
 
 View(filtered)
 
+
 #Exploratory Data Analysis
 
-#Barplot function
+# Creating a barplot (bar chart) of 8 different variables in the dataset
+# Barplot function
 create_barplot <- function(data, col_name) {
   ggplot(data, aes_string(x = col_name)) +
     geom_bar(fill = "skyblue", color = "black") +
@@ -34,13 +55,11 @@ create_barplot <- function(data, col_name) {
 }
 
 columns <- c("AGE", "EDUCATION", "HH_SIZE", "NUM_MOVES", "N_HH_P1", "N_PERS_P1", "NUM_CONT", "VAL_LOSS2_INC_P23HH")
-
 for (col in columns) {
   print(create_barplot(filtered, col))
 }
 
 #Outlier Detection
-
 #Boxplot function
 create_outlier_boxplot <- function(data, col_name) {
   ggplot(data, aes_string(y = col_name)) +
@@ -58,6 +77,8 @@ for (col in columns) {
   print(create_outlier_boxplot(filtered, col))
 }
 
+write_rds(filtered, file = here::here("dataset", "police_interaction.rds"))
+
 # Removed columns:
 # PSTRATA: don't understand how to use it
 # SECUCODE: not necessary
@@ -68,7 +89,6 @@ for (col in columns) {
 # QUESTIONS
 # There are many columns titled whyno[number]_inc_P23[HH for household crime, PER for personal crime] with 
 # description of Reason not reported: (insert reason), should I remove any of them?
-# 
 
 
 categorical_columns <- c("C4_RACE", "MALE", "MAR_STAT", "WORK_LW", "HHPOV", "FREQ_DRV", 
@@ -77,22 +97,20 @@ categorical_columns <- c("C4_RACE", "MALE", "MAR_STAT", "WORK_LW", "HHPOV", "FRE
                          "VICAR_CITIZEN", "VICAR_PRO_AUTO", "VICAR_PRO_PERS", "VICAR_OTH_CONT",
                          "VICAR_IMPROPER", "D_HH_P23", "TENURE", "MSA_STATUS", "PROPER")
   
-quantitative_columns <- c("AGE","EDUCATION", "EDUCATION_SUB", "NUM_MOVES", "NUM_CONT", "NUM_FU_HHINT", 
-                          "NUM_FU_PERINT", "N_HH_P1", "N_PERS_P1", "NUM_CITIZEN_HH", "NUM_PRO_AUTO_HH", 
+quantitative_columns <- c("AGE","EDUCATION", "EDUCATION_SUB", "NUM_MOVES", "NUM_CONT", "N_HH_P1", "N_PERS_P1", "NUM_CITIZEN_HH", "NUM_PRO_AUTO_HH", 
                           "NUM_PRO_PERS_HH", "NUM_OTH_CONT_HH", "NUM_IMPROPER_HH")
 
 filtered[quantitative_columns] <- filtered[quantitative_columns] |> 
-  map_df(~ as.numeric(.x)) |> 
+  map_df(~ as.numeric(.x))
+
+View(filtered)
+write_RDS(filtered, "filtered_data.rds")
 #   map_df(~ ifelse(is.na(.x), mean(.x, na.rm = TRUE), .x)) Commenting out this line because I'm not sure how accurate it is to replace missing values with the average value (was discussed in class)
 
+# Renaming the columns to more usable/understandable names
+# If I can't easily rename them, I will include descriptions below
+# 
+# filtered <- filtered |>
+#  rename(marital_status = MAR_STAT, employment_status = WORK_LW, years_of_education = EDUCATION, in_poverty = HHPOV, )
 
-### HIS EXAMPLE
-# This file is purely as an example.
-# Note, you may end up creating more than one cleaned data set and saving that
-# to separate files in order to work on different aspects of your project
-#library(tidyverse)
-#loan_data <- read_csv(here::here("dataset", "loan_refusal.csv"))
-## CLEAN the data
-#loan_data_clean <- loan_data |>
-#  pivot_longer(2:5, names_to = "group", values_to = "refusal_rate")
-#write_rds(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.rds"))
+
