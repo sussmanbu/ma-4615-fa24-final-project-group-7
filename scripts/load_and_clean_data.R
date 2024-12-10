3912f63f3ea82396df2b1365a2985779b1c05f21
Daniel Sussman <sussman@bu.edu>
2024-12-10 16:31:21 -0500

    Built site for gh-pages

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -1,16 +1,7 @@
-# This file is purely as an example.
-# Note, you may end up creating more than one cleaned data set and saving that
-# to separate files in order to work on different aspects of your project
-
 library(tidyverse)
 
+# Reading initial dataset
 policeContactData <- read_tsv("dataset/police-contact-data.tsv")
-#View(policeContactData)
-
-# View how many NA values each variable has
-# This information is also available online, but has been calculated here for convenience
-numNaPerVariable <- policeContactData |>
-  summarise(across(everything(), ~ sum(is.na(.)), .names = "na_count_{col}"))
 
 # Here we remove columns we don't want
 # Removed columns:
@@ -19,12 +10,8 @@ numNaPerVariable <- policeContactData |>
 # num_fu_HHint, num_fu_perint: don't need to know number of follow-ups
 # Columns that don't end with "_sub" but have a "_sub" counterpart: the "_sub" columns are objectively better -- they contain more data 
 # time2vic_inc_P23PER: don't want to investigate months from PPCS interview to victimization
-# 
-# QUESTIONS
-# There are many columns titled whyno[number]_inc_P23[HH for household crime, PER for personal crime] with 
-# description of Reason not reported: (insert reason), should I remove any of them?
 
-filtered <- policeContactData |>
+policeContactCleaned <- policeContactData |>
   select(!ends_with("_sub"), !starts_with("vicar_")) |>
   select(!c(SECUCODE, WEIGHT, NUM_FU_HHINT, NUM_FU_PERINT, PSTRATA, TIME2VIC_INC_P23PER)) |>
   mutate(
@@ -37,100 +24,42 @@ filtered <- policeContactData |>
   ) |>
   relocate(RACE, .before = AGE)
 
-View(filtered)
-
-
-#Exploratory Data Analysis
-
-# Creating a barplot (bar chart) of 8 different variables in the dataset
-# Barplot function
-create_barplot <- function(data, col_name) {
-  ggplot(data, aes_string(x = col_name)) +
-    geom_bar(fill = "skyblue", color = "black") +
-    labs(title = paste("Barplot of", col_name),
-         x = col_name,
-         y = "Count") +
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
-}
-
-columns <- c("AGE", "EDUCATION", "HH_SIZE", "NUM_MOVES", "N_HH_P1", "N_PERS_P1", "NUM_CONT", "VAL_LOSS2_INC_P23HH")
-for (col in columns) {
-  print(create_barplot(filtered, col))
-}
-
-#Outlier Detection
-#Boxplot function
-create_outlier_boxplot <- function(data, col_name) {
-  ggplot(data, aes_string(y = col_name)) +
-    geom_boxplot(outlier.color = "red",   
-                 outlier.shape = 16,      
-                 outlier.size = 2) +      
-    labs(title = paste("Boxplot of", col_name, "with Outliers"),
-         y = col_name) +
-    theme_minimal()
-}
-
-columns <- c("AGE", "EDUCATION", "HH_SIZE", "NUM_MOVES", "N_HH_P1", "N_PERS_P1", "NUM_CONT", "VAL_LOSS2_INC_P23HH") 
-
-for (col in columns) {
-  print(create_outlier_boxplot(filtered, col))
-}
-
-write_rds(filtered, file = here::here("dataset", "police_interaction.rds"))
-
-# Removed columns:
-# PSTRATA: don't understand how to use it
-# SECUCODE: not necessary
-# num_fu_HHint, num_fu_perint: don't need to know number of follow-ups
-# Columns ending with "_sub": unsure what " missing  carried back from later waves " means
-# time2vic_inc_P23PER: don't want to investigate months from PPCS interview to victimization
-# 
-# QUESTIONS
-# There are many columns titled whyno[number]_inc_P23[HH for household crime, PER for personal crime] with 
-# description of Reason not reported: (insert reason), should I remove any of them?
-
-
+# Define categorial and quantitative columns
 categorical_columns <- c("C4_RACE", "MALE", "MAR_STAT", "WORK_LW", "HHPOV", "FREQ_DRV", 
                          "HH_SIZE", "PUB_HOUSE", "PUB_HOUSE_SUB", "REGION", "PPCS_YEAR", 
                          "INPERSON", "CONTACT", "REASON", "ARRESTED", "CUFFED", "C_CONTCT", 
                          "VICAR_CITIZEN", "VICAR_PRO_AUTO", "VICAR_PRO_PERS", "VICAR_OTH_CONT",
                          "VICAR_IMPROPER", "D_HH_P23", "TENURE", "MSA_STATUS", "PROPER")
-  
+
 quantitative_columns <- c("AGE","EDUCATION", "EDUCATION_SUB", "NUM_MOVES", "NUM_CONT", "N_HH_P1", "N_PERS_P1", "NUM_CITIZEN_HH", "NUM_PRO_AUTO_HH", 
                           "NUM_PRO_PERS_HH", "NUM_OTH_CONT_HH", "NUM_IMPROPER_HH")
 
-filtered[quantitative_columns] <- filtered[quantitative_columns] |> 
+policeContactCleaned[quantitative_columns] <- policeContactCleaned[quantitative_columns] |> 
   map_df(~ as.numeric(.x))
 
-View(filtered)
-# write_RDS(filtered, "filtered_data.rds")
-#   map_df(~ ifelse(is.na(.x), mean(.x, na.rm = TRUE), .x)) Commenting out this line because I'm not sure how accurate it is to replace missing values with the average value (was discussed in class)
+# Write the cleaned dataset disk
+write_rds(policeContactCleaned, file = here::here("dataset", "police_interaction.rds"))
 
-# Renaming the columns to more usable/understandable names
-# If I can't easily rename them, I will include descriptions below
-# 
-# filtered <- filtered |>
-#  rename(marital_status = MAR_STAT, employment_status = WORK_LW, years_of_education = EDUCATION, in_poverty = HHPOV, )
-
+# Load Initial Dataset
 firearm_sh_ds <- read.csv("dataset/Firearm_suicide_homicide_dataset.csv")
 
+# Keep only relevant years, and remove irrelevant columns
 firearm_sh_ds <- firearm_sh_ds |>
   filter(year == 2002 | year == 2008 | year == 2011) |>
   select(!ends_with("fss")) |>
   select(!c(division, firearm_suicides, total_suicides))
 
+# Write cleaned version to disk
 write_rds(firearm_sh_ds, file = here::here("dataset", "firearm_sh_ds.rds"))
 
+# Standardizing the states and homicide rates and remove rows without data
 firearm_data <- firearm_sh_ds |>
   mutate(firearm_homicide_rate = as.numeric(firearm_homicide_rate),
          state = tolower(state)) |>
   filter(!is.na(firearm_homicide_rate))
 
-filtered <- read_rds("dataset/police_interaction.rds")
-
 # Combining the Two Datasets into a summary of regions
-police_sentiment_regions <- filtered |>
+police_sentiment_regions <- policeContactCleaned |>
   group_by(REGION, PPCS_YEAR) |>
   summarize(
     total_count = n(),
@@ -184,8 +113,10 @@ regional_firearm <- firearm_data |>
     homicide_rate = ((homicides / population) * 100000)
   )
 
+# Joining the two regional datasets
 combined_regional <- police_sentiment_regions |>
   full_join(regional_firearm, by = c("region", "year"))
 
+# Write final combined dataset to disk
 write_rds(combined_regional, file = here::here("dataset", "combined_regional_data.rds"))
 
